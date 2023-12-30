c4d18f3c44994e2f422264bcecd00c1fb304c8ee
Attractive Chaos <attractor@live.co.uk>
2023-12-30 01:23:20 -0500

    added n-queens in rust

---
+++ rs/nqueen.rs
@@ -0,0 +1,47 @@
+pub fn nq_solve(n: usize) -> usize {
+	const MAX_N: usize = 32;
+	let mut a: [i32; MAX_N] = [-1; MAX_N];
+	let mut l: [i32; MAX_N] = [0; MAX_N];
+	let mut c: [i32; MAX_N] = [0; MAX_N];
+	let mut r: [i32; MAX_N] = [0; MAX_N];
+	let mut m = 0;
+	let y0 = (1<<n) - 1;
+	let mut k = 0;
+	loop {
+		let y = (l[k] | c[k] | r[k]) & y0;
+		if (y ^ y0) >> (a[k] + 1) != 0 {
+			let mut i = a[k] + 1;
+			while i < n as i32 {
+				if (y & (1<<i)) == 0 {
+					break
+				}
+				i += 1;
+			}
+			if k < n - 1 {
+				let z = 1<<i;
+				a[k] = i;
+				k += 1;
+				l[k] = (l[k-1]|z)<<1;
+				c[k] = c[k-1]|z;
+				r[k] = (r[k-1]|z)>>1;
+			} else {
+				m += 1;
+				if k == 0 {
+					break;
+				}
+				k -= 1;
+			}
+		} else {
+			a[k] = -1;
+			if k == 0 {
+				break;
+			}
+			k -= 1;
+		}
+	}
+	return m;
+}
+
+fn main() {
+	println!("{}", nq_solve(15));
+}
