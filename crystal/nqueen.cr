05227f9e6d229265722c4d35f1b90ca2d88dc859
Attractive Chaos <attractor@live.co.uk>
2023-12-30 21:22:05 -0500

    added n-queens in crystal

---
+++ crystal/nqueen.cr
@@ -0,0 +1,36 @@
+def nq_solve(n)
+	a = Array.new(n) { -1 }
+	l = Array.new(n) { 0 }
+	c = Array.new(n) { 0 }
+	r = Array.new(n) { 0 }
+	y0 = (1<<n) - 1
+	m = 0
+	k = 0
+	while k >= 0
+		y = (l[k] | c[k] | r[k]) & y0
+		if (y ^ y0) >> (a[k] + 1) != 0
+			i = a[k] + 1
+			while i < n && (y & 1<<i) != 0
+				i += 1
+			end
+			if k < n - 1
+				z = 1<<i
+				a[k] = i
+				k += 1
+				l[k] = (l[k-1]|z)<<1
+				c[k] = c[k-1]|z
+				r[k] = (r[k-1]|z)>>1
+			else
+				m += 1
+				k -= 1
+			end
+		else
+			a[k] = -1
+			k -= 1
+		end
+	end
+	return m
+end
+
+n = 15
+puts nq_solve(n)
