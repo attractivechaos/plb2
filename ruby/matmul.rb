e9eef42d4b27129d5b42812d9c57bcd198ed49e7
Attractive Chaos <attractor@live.co.uk>
2024-01-01 11:28:31 -0500

    matmul in ruby

---
+++ ruby/matmul.rb
@@ -0,0 +1,38 @@
+def matgen(n)
+	tmp = 1.0 / n / n
+  	a = Array.new(n) { Array.new(n) { 0 } }
+	for i in 0 .. n-1
+		for j in 0 .. n-1
+			a[i][j] = tmp * (i - j) * (i + j)
+		end
+	end
+	return a
+end
+
+def matmul(a, b)
+	m = a.length
+	n = a[0].length
+	p = b[0].length
+  	c = Array.new(m) { Array.new(p) { 0 } }
+	for i in 0 .. m-1
+		ci = c[i]
+		for k in 0 .. n-1
+			aik = a[i][k]
+			bk = b[k]
+			for j in 0 .. p-1
+				ci[j] += aik * bk[j]
+			end
+		end
+	end
+	return c
+end
+
+n = 1000
+if ARGV.length >= 1
+	n = ARGV[0].to_i
+end
+n = n / 2 * 2
+a = matgen(n)
+b = matgen(n)
+c = matmul(a, b)
+puts c[n/2][n/2]
