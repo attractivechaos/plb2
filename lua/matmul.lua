72e63a4eb9e37b933b786db24f7fb030f96ec77f
Attractive Chaos <attractor@live.co.uk>
2024-01-01 01:28:55 -0500

    faster luajit

--- lua/matmul.lua
+++ lua/matmul.lua
@@ -1,10 +1,10 @@
 function matgen(n)
 	local tmp = 1.0 / n / n
 	local a = {}
-	for i = 0, n-1 do
+	for i = 1, n do
 		a[i] = {}
-		for j = 0, n-1 do
-			a[i][j] = tmp * (i - j) * (i + j)
+		for j = 1, n do
+			a[i][j] = tmp * (i - j) * (i + j - 2)
 		end
 	end
 	return a
@@ -12,20 +12,22 @@ end
 
 function matmul(n, a, b)
 	local c = {}
-	for i = 0, n-1 do
-		c[i] = {}
-		for j = 0, n-1 do c[i][j] = 0 end
-		for k = 0, n-1 do
-			for j = 0, n-1 do
-				c[i][j] = c[i][j] + a[i][k] * b[k][j]
+	for i = 1, n do
+		local ci = {}
+		for j = 1, n do ci[j] = 0 end
+		for k = 1, n do
+			local t, bk = a[i][k], b[k]
+			for j = 1, n do
+				ci[j] = ci[j] + t * bk[j]
 			end
 		end
+		c[i] = ci
 	end
 	return c
 end
 
-local n = 1000
+local n = tonumber(arg[1]) or 1000
 local a = matgen(n)
 local b = matgen(n)
 local c = matmul(n, a, b)
-print(c[math.floor(n/2)][math.floor(n/2)]);
+print(c[math.floor(n/2)+1][math.floor(n/2)+1]);
