168e434683932dc09172488b2b2bbdebe1554798
Attractive Chaos <attractor@live.co.uk>
2024-01-01 01:47:26 -0500

    added matmul in julia

---
+++ julia/matmul.jl
@@ -0,0 +1,26 @@
+function matgen(n)
+	a = Array{Float64}(undef, n, n)
+	tmp = 1.0 / n / n
+	for i = 1:n, j = 1:n
+		a[i,j] = tmp * (i - j) * (i + j - 2)
+	end
+	return a
+end
+
+function matmul(n, a, b)
+	c = zeros(Float64, n, n)
+	for i = 1:n
+		for k = 1:n
+			for j = 1:n
+				c[i,j] += a[i,k] * b[k,j]
+			end
+		end
+	end
+	return c
+end
+
+n = 1000
+a = matgen(n)
+b = matgen(n)
+c = matmul(n, a, b)
+println(c[(n>>1)+1,(n>>1)+1])
