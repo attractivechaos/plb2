2ca10a34e9cca48aa47b0142a87e1b6911c126d0
spence <spence@users.noreply.github.com>
2024-01-03 00:16:35 -0800

    rust matmul: static memory allocation using const generics

--- src/rust/matmul.rs
+++ src/rust/matmul.rs
@@ -1,8 +1,8 @@
-fn matgen(n: usize) -> Vec<Vec<f64>> {
-	let mut a = vec![vec![0f64; n]; n];
-	let tmp = 1.0 / (n as f64) / (n as f64);
-	for i in 0..n {
-		for j in 0..n {
+fn matgen<const N: usize>() -> Box<[[f64; N]; N]> {
+	let mut a = Box::new([[0f64; N]; N]);
+	let tmp = 1.0 / (N as f64) / (N as f64);
+	for i in 0..N {
+		for j in 0..N {
 			let ii = i as i64;
 			let jj = j as i64;
 			a[i][j] = tmp * ((ii - jj) as f64) * ((ii + jj) as f64);
@@ -11,12 +11,12 @@ fn matgen(n: usize) -> Vec<Vec<f64>> {
 	return a;
 }
 
-fn matmul(n: usize, a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-	let mut c = vec![vec![0f64; n]; n];
-	for i in 0..n {
-		for k in 0..n {
+fn matmul<const N: usize>(a: &[[f64; N]; N], b: &[[f64; N]; N]) -> Box<[[f64; N]; N]> {
+	let mut c = Box::new([[0f64; N]; N]);
+	for i in 0..N {
+		for k in 0..N {
 			let aik = a[i][k]; // hoisting aik helps performance, but hoisting b[k] doesn't
-			for j in 0..n {
+			for j in 0..N {
 				c[i][j] += aik * b[k][j];
 			}
 		}
@@ -25,9 +25,9 @@ fn matmul(n: usize, a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
 }
 
 fn main() {
-	let n = 1500;
-	let a = matgen(n);
-	let b = matgen(n);
-	let c = matmul(n, a, b);
-	println!("{}", c[n>>1][n>>1]);
+	const N: usize = 1500;
+	let a = matgen::<N>();
+	let b = matgen::<N>();
+	let c = matmul::<N>(&a, &b);
+	println!("{}", c[N>>1][N>>1]);
 }
