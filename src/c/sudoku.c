e1663ca9bddede2035d1c2c539e0dfcb5bb93fd8
Attractive Chaos <attractor@live.co.uk>
2024-01-03 21:08:09 -0500

    code cleanup; no effect on timing

--- src/c/sudoku.c
+++ src/c/sudoku.c
@@ -1,6 +1,6 @@
 /* The MIT License
 
-   Copyright (c) 2011 by Attractive Chaos <attractor@live.co.uk>
+   Copyright (c) 2011-2023 by Attractive Chaos <attractor@live.co.uk>
 
    Permission is hereby granted, free of charge, to any person obtaining
    a copy of this software and associated documentation files (the
@@ -88,7 +88,7 @@ sdaux_t *sd_genmat()
 	sdaux_t *a;
 	int i, j, k, r, c, c2, r2;
 	int8_t nr[324];
-	a = calloc(1, sizeof(sdaux_t));
+	a = (sdaux_t*)calloc(1, sizeof(sdaux_t));
 	for (i = r = 0; i < 9; ++i) // generate c[729][4]
 		for (j = 0; j < 9; ++j)
 			for (k = 0; k < 9; ++k) // this "9" means each cell has 9 possible numbers
@@ -103,36 +103,44 @@ sdaux_t *sd_genmat()
 			k = a->c[r][c2], a->r[k][nr[k]++] = r;
 	return a;
 }
-// update the state vectors when we pick up choice r; v=1 for setting choice; v=-1 for reverting
-static inline int sd_update(const sdaux_t *aux, int8_t sr[729], uint8_t sc[324], int r, int v)
+// update the state vectors when we pick up choice r; set the choice
+static inline int sd_update_for(const sdaux_t *aux, int8_t sr[729], uint8_t sc[324], int r)
 {
 	int c2, min = 10, min_c = 0;
-	for (c2 = 0; c2 < 4; ++c2) sc[aux->c[r][c2]] += v<<7;
+	for (c2 = 0; c2 < 4; ++c2) sc[aux->c[r][c2]] |= 1U<<7;
 	for (c2 = 0; c2 < 4; ++c2) { // update # available choices
-		int r2, rr, cc2, c = aux->c[r][c2];
-		if (v > 0) { // move forward
-			for (r2 = 0; r2 < 9; ++r2) {
-				if (sr[rr = aux->r[c][r2]]++ != 0) continue; // update the row status
-				for (cc2 = 0; cc2 < 4; ++cc2) {
-					int cc = aux->c[rr][cc2];
-					if (--sc[cc] < min) // update # allowed choices
-						min = sc[cc], min_c = cc; // register the minimum number
-				}
-			}
-		} else { // revert
-			const uint16_t *p;
-			for (r2 = 0; r2 < 9; ++r2) {
-				if (--sr[rr = aux->r[c][r2]] != 0) continue; // update the row status
-				p = aux->c[rr]; ++sc[p[0]]; ++sc[p[1]]; ++sc[p[2]]; ++sc[p[3]]; // update the count array
+		int r2, c = aux->c[r][c2];
+		for (r2 = 0; r2 < 9; ++r2) {
+			int rr = aux->r[c][r2], cc2;
+			if (sr[rr]++ != 0) continue; // update the row status
+			for (cc2 = 0; cc2 < 4; ++cc2) {
+				int cc = aux->c[rr][cc2];
+				if (--sc[cc] < min) // update # allowed choices
+					min = sc[cc], min_c = cc; // register the minimum number
 			}
 		}
 	}
 	return min<<16 | min_c; // return the col that has been modified and with the minimal available choices
 }
+// update but revert the choice
+static inline void sd_update_rev(const sdaux_t *aux, int8_t sr[729], uint8_t sc[324], int r)
+{
+	int c2;
+	for (c2 = 0; c2 < 4; ++c2) sc[aux->c[r][c2]] &= 0x7f;
+	for (c2 = 0; c2 < 4; ++c2) { // update # available choices
+		int r2, cc2, c = aux->c[r][c2];
+		const uint16_t *p;
+		for (r2 = 0; r2 < 9; ++r2) {
+			int rr = aux->r[c][r2];
+			if (--sr[rr] != 0) continue; // update the row status
+			p = aux->c[rr]; ++sc[p[0]]; ++sc[p[1]]; ++sc[p[2]]; ++sc[p[3]]; // update the count array
+		}
+	}
+}
 // solve a Sudoku; _s is the standard dot/number representation
 int sd_solve(const sdaux_t *aux, const char *_s)
 {
-	int i, j, r, c, r2, dir, cand, n = 0, min, hints = 0; // dir=1: forward; dir=-1: backtrack
+	int i, r, c, dir, cand, n = 0, hints = 0; // dir=1: forward; dir=-1: backtrack
 	int8_t sr[729], cr[81]; // sr[r]: # times the row is forbidden by others; cr[i]: row chosen at step i
 	uint8_t sc[324]; // bit 1-7: # allowed choices; bit 8: the constraint has been used or not
 	int16_t cc[81]; // cc[i]: col chosen at step i
@@ -141,14 +149,22 @@ int sd_solve(const sdaux_t *aux, const char *_s)
 	for (c = 0; c < 324; ++c) sc[c] = 0<<7|9; // 9 allowed choices; no constraint has been used
 	for (i = 0; i < 81; ++i) {
 		int a = _s[i] >= '1' && _s[i] <= '9'? _s[i] - '1' : -1; // number from -1 to 8
-		if (a >= 0) sd_update(aux, sr, sc, i * 9 + a, 1); // set the choice
+		if (a >= 0) sd_update_for(aux, sr, sc, i * 9 + a); // set the choice
 		if (a >= 0) ++hints; // count the number of hints
-		cr[i] = cc[i] = -1, out[i] = _s[i];
+		cr[i] = cc[i] = -1;
+		out[i] = _s[i];
 	}
-	for (i = 0, dir = 1, cand = 10<<16|0, out[81] = 0;;) {
+	out[81] = 0;
+	dir = 1;
+	cand = 10<<16|0;
+	i = 0;
+	for (;;) {
+		int j;
 		while (i >= 0 && i < 81 - hints) { // maximum 81-hints steps
+			int r2;
 			if (dir == 1) {
-				min = cand>>16, cc[i] = cand&0xffff;
+				int min = cand>>16;
+				cc[i] = cand&0xffff;
 				if (min > 1) {
 					for (c = 0; c < 324; ++c) {
 						if (sc[c] < min) {
@@ -157,19 +173,22 @@ int sd_solve(const sdaux_t *aux, const char *_s)
 						}
 					}
 				}
-				if (min == 0 || min == 10) cr[i--] = dir = -1; // backtrack
+				if (min == 0 || min == 10)
+					cr[i--] = dir = -1; // backtrack
 			}
 			c = cc[i];
-			if (dir == -1 && cr[i] >= 0) sd_update(aux, sr, sc, aux->r[c][cr[i]], -1); // revert the choice
+			if (dir == -1 && cr[i] >= 0)
+				sd_update_rev(aux, sr, sc, aux->r[c][cr[i]]); // revert the choice
 			for (r2 = cr[i] + 1; r2 < 9; ++r2) // search for the choice to make
 				if (sr[aux->r[c][r2]] == 0) break; // found if the state equals 0
 			if (r2 < 9) {
-				cand = sd_update(aux, sr, sc, aux->r[c][r2], 1); // set the choice
+				cand = sd_update_for(aux, sr, sc, aux->r[c][r2]); // set the choice
 				cr[i++] = r2; dir = 1; // moving forward
 			} else cr[i--] = dir = -1; // backtrack
 		}
 		if (i < 0) break;
-		for (j = 0; j < i; ++j) r = aux->r[cc[j]][cr[j]], out[r/9] = r%9 + '1'; // print
+		for (j = 0; j < i; ++j)
+			r = aux->r[cc[j]][cr[j]], out[r/9] = r%9 + '1'; // print
 		puts(out);
 		++n; --i; dir = -1; // backtrack
 	}
