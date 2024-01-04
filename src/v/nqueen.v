6ce9126524ce228bc90d30ab547a0e2b664e79bb
Yurii Khomenko <yykhomenko@gmail.com>
2024-01-04 01:26:18 +0100

    [pedantic] v apply v fmt (code format tool)

--- src/v/nqueen.v
+++ src/v/nqueen.v
@@ -5,25 +5,25 @@ fn nq_solve(n int) int {
 	mut c := []int{len: n, init: 0}
 	mut r := []int{len: n, init: 0}
 	mut m := 0
-	y0 := (1<<n) - 1
+	y0 := (1 << n) - 1
 	mut k := 0
 	for k >= 0 {
 		y := (l[k] | c[k] | r[k]) & y0
 		if (y ^ y0) >> (a[k] + 1) != 0 {
 			mut i := a[k] + 1
 			for i < n {
-				if (y & (1<<i)) == 0 {
+				if (y & (1 << i)) == 0 {
 					break
 				}
 				i += 1
 			}
 			if k < n - 1 {
-				z := u32(1)<<i
+				z := u32(1) << i
 				a[k] = i
 				k += 1
-				l[k] = (l[k-1]|z)<<1
-				c[k] = c[k-1]|z
-				r[k] = (r[k-1]|z)>>1
+				l[k] = (l[k - 1] | z) << 1
+				c[k] = c[k - 1] | z
+				r[k] = (r[k - 1] | z) >> 1
 			} else {
 				m += 1
 				k -= 1
