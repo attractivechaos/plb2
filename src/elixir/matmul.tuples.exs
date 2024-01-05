c2ad00b5d730a8c2b27467848e5c8c9b159e0623
Corey Powell <mistdragon100@gmail.com>
2024-01-04 21:46:27 -0500

    Fixed elixir matmul implementation and tweaked for performance

--- src/elixir/matmul.tuples.exs
+++ src/elixir/matmul.tuples.exs
@@ -1,35 +1,37 @@
 #!/usr/bin/env -S mix run --no-mix-exs
-new_tuple = fn
-  new_tuple, y, x, cb, acc when x >= 0 ->
-    new_tuple.(new_tuple, y, x - 1, cb, [cb.(y, x) | acc])
-
-  _new_tuple, _y, x, _cb, acc when x < 0 ->
-    # important note here, this needs to be reversed since the tuple
-    # creation counts down instead of up
-    List.to_tuple(Enum.reverse(acc))
+matgen = fn h, w, cb ->
+  List.to_tuple(Enum.map(0..(h-1), fn y ->
+    List.to_tuple(Enum.map(0..(w-1), fn x ->
+      cb.(y, x)
+    end))
+  end))
 end
 
-matgen = fn h, w, cb_or_val ->
-  List.to_tuple(for y <- 0..(h-1) do
-    new_tuple.(new_tuple, y, w-1, cb_or_val, [])
-  end)
+zipmul = fn
+  _zipmul, _ac, [], [], acc ->
+    Enum.reverse(acc)
+
+  zipmul, ac, [c | cr], [b | br], acc ->
+    zipmul.(zipmul, ac, cr, br, [c + ac * b | acc])
 end
 
 matmul = fn a, ah, aw, b, _bh, bw ->
   Enum.reduce(0..(ah-1), [], fn ay, acc ->
+    ayr = elem(a, ay)
     # So what we're doing here is constructing a single row of the result (c)
     # since we only need to operate on a single row at a time here
-    cyr = List.to_tuple(Enum.map(0..(bw - 1), fn _ ->
+    cyr = Enum.map(0..(bw-1), fn _ ->
       0
-    end))
+    end)
 
-    [Enum.reduce(0..(aw-1), cyr, fn ax, cyr ->
-      ac = elem(elem(a, ay), ax)
-      byr = elem(b, ax)
-      Enum.reduce(0..(bw-1), cyr, fn bx, cyr ->
-        put_elem(cyr, bx, ac * elem(byr, bx))
+    cyr =
+      Enum.reduce(0..(aw-1), cyr, fn ax, cyr ->
+        ac = elem(ayr, ax)
+        byr = Tuple.to_list(elem(b, ax))
+        zipmul.(zipmul, ac, cyr, byr, [])
       end)
-    end) | acc]
+
+    [List.to_tuple(cyr) | acc]
   end)
   # We built the matrix in reverse, so we need to reverse the list again
   |> Enum.reverse()
@@ -38,14 +40,14 @@ matmul = fn a, ah, aw, b, _bh, bw ->
 end
 
 n = 1500
-# n = 400 # for quicker testing
+#n = 400 # for quicker testing
 tmp = 1.0 / n / n
 init_mat = fn y, x ->
   tmp * (y - x) * (y + x)
 end
 
 n = floor(n / 2) * 2
-{elapsed, x} = :timer.tc(fn ->
+{_elapsed, x} = :timer.tc(fn ->
   a = matgen.(n, n, init_mat)
   b = matgen.(n, n, init_mat)
   c = matmul.(a, n, n, b, n, n)
@@ -53,4 +55,4 @@ n = floor(n / 2) * 2
   elem(elem(c, floor(n/2)), floor(n/2))
 end)
 
-IO.inspect {elapsed / 1_000_000, x}
+IO.inspect x
