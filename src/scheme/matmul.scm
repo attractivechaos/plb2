30b85eb4ba7f171dcb16170a759c1b7f11ba884f
Alex Arslan <ararslan@comcast.net>
2024-01-06 19:30:59 -0800

    Implement benchmarks in Chez Scheme (#55)
    This includes nqueen and matmul only for the moment. I'm not sure what
    it would take to make the code here portable to other Scheme
    implementations; I focused only on Chez (and not Petite Chez).
    
    To execute a particular file, use
    ```
    chez --optimize-level 3 --program <file>
    ```

---
+++ src/scheme/matmul.scm
@@ -0,0 +1,63 @@
+(import (chezscheme))
+
+; simple vector-backed, column-major matrix type
+(define-record-type matrix
+  (fields
+    (immutable data)
+    (immutable rows)
+    (immutable columns))
+  (protocol
+    (lambda (new)
+      (lambda (rows columns fill)
+        (new (make-vector (fx* rows columns) fill) rows columns)))))
+
+(define (matrix-length a)
+  (vector-length (matrix-data a)))
+
+(define (linear-index a i j)
+  (fx+ i (fx* j (matrix-rows a))))
+
+(define (subscripts a k)
+  (let-values ([(j i) (fxdiv-and-mod k (matrix-rows a))])
+    (values i j)))
+
+(define (matrix-ref a i j)
+  (vector-ref (matrix-data a)
+              (linear-index a i j)))
+
+(define (matrix-set! a i j v)
+  (vector-set! (matrix-data a)
+               (linear-index a i j)
+               v))
+
+(define (matrix-generate n)
+  (let* ([m (fixnum->flonum n)]
+         [a (make-matrix n n (fl/ 1.0 m m))]
+         [p (matrix-length a)])
+    (do ([k 0 (fx+ k 1)])
+      ((fx>= k p) a)
+      (let-values ([(i j) (subscripts a k)])
+        (matrix-set! a i j (fl* (matrix-ref a i j)
+                                (fixnum->flonum (fx- i j))
+                                (fixnum->flonum (fx+ i j))))))))
+
+(define (matrix-multiply a b)
+  (let* ([n (matrix-rows a)]
+         [p (matrix-columns a)]
+         [m (matrix-columns b)]
+         [c (make-matrix n m 0.0)])
+    (do ([j 0 (fx+ j 1)])
+      ((fx>= j m) c)
+      (do ([k 0 (fx+ k 1)])
+        ((fx>= k p))
+        (do ([i 0 (fx+ i 1)])
+          ((fx>= i n))
+          (matrix-set! c i j (fl+ (matrix-ref c i j)
+                                  (fl* (matrix-ref a i k)
+                                       (matrix-ref b k j)))))))))
+
+(let* ([n 1500]
+       [i (fxsrl n 1)])
+  (display (matrix-ref (matrix-multiply (matrix-generate n)
+                                        (matrix-generate n))
+                       i i)))
