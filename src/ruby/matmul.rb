bcc04cfbbfef38a54289e918c227770fac5c9594
Gopal Patel <nixme@users.noreply.github.com>
2024-01-10 12:07:28 -0800

    Ruby matmul perf improvement (#62)

--- src/ruby/matmul.rb
+++ src/ruby/matmul.rb
@@ -1,38 +1,45 @@
+# frozen_string_literal: true
+
 def matgen(n)
-	tmp = 1.0 / n / n
-  	a = Array.new(n) { Array.new(n) { 0 } }
-	for i in 0 .. n-1
-		for j in 0 .. n-1
-			a[i][j] = tmp * (i - j) * (i + j)
-		end
-	end
-	return a
+  tmp = 1.0 / n / n
+  a = Array.new(n) { Array.new(n, 0.0) }
+  for i in 0...n
+    for j in 0...n
+      a[i][j] = tmp * (i - j) * (i + j)
+    end
+  end
+  a
 end
 
 def matmul(a, b)
-	m = a.length
-	n = a[0].length
-	p = b[0].length
-  	c = Array.new(m) { Array.new(p) { 0 } }
-	for i in 0 .. m-1
-		ci = c[i]
-		for k in 0 .. n-1
-			aik = a[i][k]
-			bk = b[k]
-			for j in 0 .. p-1
-				ci[j] += aik * bk[j]
-			end
-		end
-	end
-	return c
+  m = a.length
+  n = a[0].length
+  p = b[0].length
+  c = Array.new(m) { Array.new(p, 0.0) }
+  for i in 0...m
+    ci = c[i]
+    ai = a[i]
+    k = 0
+    while k < n
+      aik = ai[k]
+      bk = b[k]
+      j = 0
+      while j < p
+        ci[j] += aik * bk[j]
+        j += 1
+      end
+      k += 1
+    end
+  end
+  c
 end
 
 n = 1500
-if ARGV.length >= 1
-	n = ARGV[0].to_i
+if ARGV.length > 0
+  n = ARGV[0].to_i
 end
 n = n / 2 * 2
 a = matgen(n)
 b = matgen(n)
 c = matmul(a, b)
-puts c[n/2][n/2]
+puts c[n / 2][n / 2]
